7bd93e71e2cf6aa344560bae2bb4ea6933b96c3a
Kaique Covo <kaique.kng@gmail.com>
2020-07-09 03:42:43 -0300

    [New] - CreateSession test

---
+++ src/modules/users/services/CreateSessionService.spec.ts
@@ -0,0 +1,70 @@
+import CreateSessionService from './CreateSessionService'
+import CreateUserService from './CreateUserService'
+
+/** Fake provider */
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+
+/** Fake Repository */
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+
+/** Shared */
+import AppError from '@shared/errors/AppError'
+
+describe('CreateSessionService', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsers = new FakeUsersRepository()
+    const fakeHash = new FakeHashProvider()
+
+    const createUser = new CreateUserService(fakeUsers, fakeHash)
+    const createSession = new CreateSessionService(fakeUsers, fakeHash)
+
+    const user = await createUser.execute({
+      name: 'John doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const userAuthenticated = await createSession.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(userAuthenticated).toHaveProperty('token')
+    expect(userAuthenticated.user).toEqual(user)
+  })
+
+  it('should not be able to authenticate email that does not exists', async () => {
+    const fakeUsers = new FakeUsersRepository()
+    const fakeHash = new FakeHashProvider()
+
+    const createSession = new CreateSessionService(fakeUsers, fakeHash)
+
+    const user = createSession.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('it should not be able to authenticate user with wrong password', async () => {
+    const fakeUsers = new FakeUsersRepository()
+    const fakeHash = new FakeHashProvider()
+
+    const createUser = new CreateUserService(fakeUsers, fakeHash)
+    const createSession = new CreateSessionService(fakeUsers, fakeHash)
+
+    await createUser.execute({
+      name: 'John doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const userWithWrongPassword = createSession.execute({
+      email: 'johndoe@example.com',
+      password: 'wrongPassword',
+    })
+
+    expect(userWithWrongPassword).rejects.toBeInstanceOf(AppError)
+  })
+})
